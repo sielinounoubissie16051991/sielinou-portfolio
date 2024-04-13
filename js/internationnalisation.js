ff282adc1561eb8c93f072d9ed5332cbf5682c54
sielinounoubissie16051991 <sielinou1605@gmail.com>
2024-04-13 17:55:42 +0100

    reactualisation du Typed('.multiple-text', {             strings: ['Frontend Developer', 'UI/UX Designer', 'Freelancer'],             typeSpeed: 100,             backSpeed: 100,             backDelay: 1000,             loop: true,         }) dans la fonction changeLanguage() de internationnalisation.js pour maintenir fonctionnel (annimer l'afficheage de mes fonctions annimé) de mes fonction

--- js/internationnalisation.js
+++ js/internationnalisation.js
@@ -3,10 +3,10 @@ function loadLanguage(lang) {
     document.addEventListener('DOMContentLoaded', function () {
         let fr_en = document.getElementById('fr_en');
         console.log(lang);
-        if (lang == 'en') {
+        if (lang === 'en') {
             console.log(lang);
             fr_en.innerHTML = `<button class="" onclick="changeLanguage('fr')" id="Français">Français</button>`
-        } else if (lang == 'fr') {
+        } else if (lang === 'fr') {
             console.log(lang);
             fr_en.innerHTML = `<button class="" onclick="changeLanguage('en')" id="English">English</button>`;
         }
@@ -15,89 +15,45 @@ function loadLanguage(lang) {
         .then(response => response.json());
 }
 
-    // Mettre à jour le contenu de la page en fonction de la langue
-function updateContent(data) {
-    console.log(data);
-    document.getElementById('Accueil').innerText = data.Accueil;
-    document.getElementById('Apropos_1').innerText = data.Apropos_1;
-    document.getElementById('Services_1').innerText = data.Services_1;
-    document.getElementById('Portfolio_1').innerText = data.Portfolio_1;
-    document.getElementById('Contact_1').innerText = data.Contact_1;
-    document.getElementById('SALUT').innerText = data.SALUT;
-    document.getElementById('NOM_PRENOM').innerText = data.NOM_PRENOM;
-    //document.getElementById('JE_SUIS').innerText = data.JE_SUIS;
-    document.getElementById('RESAU_SOCIAL').innerText = data.RESAU_SOCIAL;
-    //document.getElementById('CV').innerText = data.CV;
-    document.getElementById('Apropos_2').innerText = data.Apropos_2;
-    //console.log('moi', document.querySelector("#MOI"));
-    document.getElementById('MOI').innerText = data.MOI;
-   // document.getElementById('Frontend_Developer').innerText = data.Frontend_Developer;
-    document.getElementById('DEMARQUATION').innerText = data.DEMARQUATION;
-    document.getElementById('PARCOURS').innerText = data.PARCOURS;
-    document.getElementById('VOIR_PLUS_1').innerText = data.VOIR_PLUS_1;
-    document.getElementById('Mes').innerText = data.Mes;
-    document.getElementById('Services_2').innerText = data.Services_2;
-    document.getElementById('Developement_Web').innerText = data.Developement_Web;
-    document.getElementById('JEUNE_DEV_1').innerText = data.JEUNE_DEV_1;
-    document.getElementById('VOIR_PLUS_2').innerText = data.VOIR_PLUS_2;
-    document.getElementById('Graphiste').innerText = data.Graphiste;
-    document.getElementById('DEBUTANT').innerText = data.DEBUTANT;
-    document.getElementById('VOIR_PLUS_3').innerText = data.VOIR_PLUS_3;
-    document.getElementById('UI_UX').innerText = data.UI_UX;
-    document.getElementById('JEUNE_TALENT').innerText = data.JEUNE_TALENT;
-    document.getElementById('VOIR_PLUS_4').innerText = data.VOIR_PLUS_4;
-    document.getElementById('Mes_dernier').innerText = data.Mes_dernier;
-    document.getElementById('Projets_1').innerText = data.Projets_1;
-    document.getElementById('Mes_projets').innerText = data.Mes_projets;
-    document.getElementById('Personnel').innerText = data.Personnel;
-    document.getElementById('Web_Design_1').innerText = data.Web_Design_1;
-    document.getElementById('HEBERGEMENT').innerText = data.HEBERGEMENT;
-    document.getElementById('Web_Design_2').innerText = data.Web_Design_2;
-    document.getElementById('RESTEAURANT').innerText = data.RESTEAURANT;
-    document.getElementById('Web_Design_3').innerText = data.Web_Design_3;
-    document.getElementById('PHOTOGRAPHE').innerText = data.PHOTOGRAPHE;
-    document.getElementById('Web_Design_4').innerText = data.Web_Design_4;
-    document.getElementById('PETITS_PLATS').innerText = data.PETITS_PLATS;
-    document.getElementById('PROJET').innerText = data.PROJET;
-    console.log(document.getElementById('PROJET').firstChild);
-    document.getElementById('PROFESSIONNEL').innerText = data.PROFESSIONNEL;
-    document.getElementById('CONTACT_FORM').innerText = data.CONTACT_FORM;
-    document.getElementById('Me_').innerText = data.Me_;
-    //document.getElementById('Français').innerText = data.Français;
-    //document.getElementById('English').innerText = data.English;
+// Mettre à jour le contenu de la page en fonction de la langue
+// Changer la langue et mettre à jour le contenu
+function changeLanguage(lang) {
+    localStorage.setItem('langue', lang);
+    console.log(lang);
+    let version_fr_or_en = document.getElementById('version_fr_or_en');
+    if (lang === 'en') {
+        version_fr_or_en.innerHTML = genereCodeHTMLEn();
+        let fr_en = document.getElementById('fr_en');
+        fr_en.innerHTML = `<button class="" onclick="changeLanguage('fr')" id="Français">Français</button>`
+        let Personnel = document.getElementById('Personnel');
+        Personnel.style.marginRight = '1rem';
+        let multipleText = document.querySelector('.multiple-text');
+        multipleText.style.marginLeft = '1rem';
+        new Typed('.multiple-text', {
+            strings: ['Frontend Developer', 'UI/UX Designer', 'Freelancer'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true,
+        });
+    } else if (lang === 'fr') {
+        version_fr_or_en.innerHTML = genereCodeHTMLFr();
+        let fr_en = document.getElementById('fr_en');
+        fr_en.innerHTML = `<button class="" onclick="changeLanguage('en')" id="English">English</button>`;
+        new Typed('.multiple-text', {
+            strings: ['Frontend Developer', 'UI/UX Designer', 'Freelancer'],
+            typeSpeed: 100,
+            backSpeed: 100,
+            backDelay: 1000,
+            loop: true,
+        });
+    }
+    /*
+    loadLanguage(lang)
+        .then(data => updateContent(data))
+        .catch(error => console.error('Erreur de chargement du fichier JSON', error));
+        */
+
 }
-                // Changer la langue et mettre à jour le contenu
-                function changeLanguage(lang) {
-                    localStorage.setItem('langue', lang);
-                    /*
-                    console.log(lang);
-                    if (lang == 'en') {
-                        console.log(lang);
-                        fr_en.innerHTML = `<button class="" onclick="changeLanguage('fr')" id="Français">Français</button>`
-                    } else if (lang == 'fr') {
-                        console.log(lang);
-                        fr_en.innerHTML = `<button class="" onclick="changeLanguage('en')" id="English">English</button>`;
-                    }
-                    */
-                    console.log(lang);
-                    let version_fr_or_en = document.getElementById('version_fr_or_en');
-                    if (lang == 'en') {
-                        version_fr_or_en.innerHTML = genereCodeHTMLEn();
-                        let fr_en = document.getElementById('fr_en');
-                        let Personnel = document.getElementById('Personnel');
-                        Personnel.style.marginRight = '1rem';
-                        fr_en.innerHTML = `<button class="" onclick="changeLanguage('fr')" id="Français">Français</button>`
-                    } else if (lang == 'fr') {
-                        version_fr_or_en.innerHTML = genereCodeHTMLFr();
-                        let fr_en = document.getElementById('fr_en');
-                        fr_en.innerHTML = `<button class="" onclick="changeLanguage('en')" id="English">English</button>`;
-                    } 
-                    /*
-                    loadLanguage(lang)
-                        .then(data => updateContent(data))
-                        .catch(error => console.error('Erreur de chargement du fichier JSON', error));
-                        */
-                        
-                }
 // Chargement initial en français
-    changeLanguage('fr');
+changeLanguage('fr');
--- js/script.js
+++ js/script.js
@@ -42,40 +42,40 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 };
 
-    /*======================    scroll reveal   ===================== */
-    ScrollReveal({ 
-        // reset: true,
-        distance: '80px',
-        duration: 2000,
-        deplay: 200
-     });
-     ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-     ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact, form', { origin: 'bottom' });
-     ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-     ScrollReveal().reveal('.home-content p, .about-content', { origin: 'rigth' });
+/*======================    scroll reveal   ===================== */
+// ScrollReveal({
+//     // reset: true,
+//     distance: '80px',
+//     duration: 2000,
+//     deplay: 200
+// });
+// ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+// ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact, form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+// ScrollReveal().reveal('.home-content p, .about-content', { origin: 'rigth' });
 
-     /*=======================      typed js    ====================*/
+/*=======================      typed js    ====================*/
 
-    const typed = new Typed('.multiple-text', {
-        strings: ['Frontend Developer', 'UI/UX Designer', 'Freelancer'],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true,
-    });
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'UI/UX Designer', 'Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+});
 
-     /*=======================      see more    ====================*/
-     /*
-    function toggleText() {
-        var textContainer = document.querySelector('.about-content');
-        var buttonText = document.querySelector('.btn-VOIR_PLUS_1');
+/*=======================      see more    ====================*/
+/*
+function toggleText() {
+   var textContainer = document.querySelector('.about-content');
+   var buttonText = document.querySelector('.btn-VOIR_PLUS_1');
 
-        if (textContainer.style.display === 'none') {
-            textContainer.style.display = 'block';
-            buttonText.innerText = 'Voir moins';
-        } else {
-            textContainer.style.display = 'none';
-            buttonText.innerText = 'Voir plus';
-        }
-    }
-    */
+   if (textContainer.style.display === 'none') {
+       textContainer.style.display = 'block';
+       buttonText.innerText = 'Voir moins';
+   } else {
+       textContainer.style.display = 'none';
+       buttonText.innerText = 'Voir plus';
+   }
+}
+*/
